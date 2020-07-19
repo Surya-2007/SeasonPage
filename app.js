c18bb1a6c8d670d4e2d41119f98d7fd89a8a97bf
Surya-2007 <67518090+Surya-2007@users.noreply.github.com>
2020-07-19 10:16:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+function copy() {
+  alert("LMAO!!! Lazy couch potato.Seriously just Type it");
+}
+
+function summer() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/dkd5jyhic/image/upload/v1594968327/Summer_tf9yzu.png')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "100%";
+  alert("it is summer");
+}
+
+function winter() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "100%";
+}
+
+function autumn() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "100%";
+}
+
+function spring() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+  document.body.style.backgroundRepeat - "no-repeat";
+  document.body.style.backgroundSize = "100%";
+}
